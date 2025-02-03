b9868e027c33182a82db51901f6ff9de7e8ddf68
Marek Zelinka <mzelinka17@gmail.com>
2025-02-03 10:07:10 +0100

    sort completed sessions

--- src/components/session-tracker.tsx
+++ src/components/session-tracker.tsx
@@ -1,4 +1,5 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { isSameDay } from "@/lib/dates";
 import type { CompletedSession } from "@/types";
 import { Fragment } from "react";
 import { Separator } from "./ui/separator";
@@ -20,32 +21,51 @@ export function SessionTracker({ sessions }: { sessions: CompletedSession[] }) {
   );
 
   return (
-    <ScrollArea className="h-32">
-      {sortedSessions.map((session, index) => (
-        <Fragment key={index}>
-          <div className="text-muted-foreground space-y-1 text-xs">
-            <p>
-              Completed at{" "}
-              <time
-                dateTime={session.completedAt.toISOString()}
-                className="text-foreground"
-              >
-                {dateFormatter.format(session.completedAt)}
-              </time>
-            </p>
-            <p>
-              Started at{" "}
-              <time
-                dateTime={session.createdAt.toISOString()}
-                className="text-foreground"
-              >
-                {dateFormatter.format(session.createdAt)}
-              </time>
-            </p>
-          </div>
-          {index < sessions.length - 1 ? <Separator className="my-4" /> : null}
-        </Fragment>
-      ))}
+    <ScrollArea className="h-32 text-xs">
+      {sortedSessions.map((session, index) => {
+        const isCompletedToday = isSameDay(session.completedAt, new Date());
+
+        return (
+          <Fragment key={index}>
+            <div className="text-muted-foreground space-y-1">
+              <p>
+                Completed at{" "}
+                <time
+                  dateTime={session.completedAt.toISOString()}
+                  className="text-foreground"
+                >
+                  {dateFormatter.format(session.completedAt)}
+                </time>
+              </p>
+              <p>
+                Started at{" "}
+                <time
+                  dateTime={session.createdAt.toISOString()}
+                  className="text-foreground"
+                >
+                  {dateFormatter.format(session.createdAt)}
+                </time>
+              </p>
+            </div>
+            {index < sessions.length - 1 ? (
+              !isCompletedToday ? (
+                <div className="relative my-4">
+                  <div className="absolute inset-0 flex items-center">
+                    <Separator className="bg-destructive/20" />
+                  </div>
+                  <div className="relative flex justify-start">
+                    <span className="text-destructive bg-white pr-2">
+                      Older than today
+                    </span>
+                  </div>
+                </div>
+              ) : (
+                <Separator className="my-4" />
+              )
+            ) : null}
+          </Fragment>
+        );
+      })}
     </ScrollArea>
   );
 }
